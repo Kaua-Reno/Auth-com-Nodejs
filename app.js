10a67f0cf38274fb7352c1a3113defacd832b312
Kauã <kaua.reno@gmail.com>
2023-03-26 16:55:52 -0300

    Código com autentificação de token

--- app.js
+++ app.js
@@ -18,6 +18,39 @@ app.get('/', (req, res) => {
     res.status(200).json({ msg: 'Bem vindo a nossa API'})
 })
 
+// Private Route
+app.get("/user/:id", checkToken, async (req, res) => {
+    const id = req.params.id
+
+    // check if user exists
+    const user = await User.findById(id, '-password')
+
+    if(!user) {
+        return res.status(404).json({ msg: 'Usuário não encontrado '})
+    }
+
+    res.status(200).json({ user })
+})
+
+function checkToken(req, res, nest) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if (!token) {
+        return res.status(401).json({msg: "Acesso negado"})
+    }
+
+    try {
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+    } catch (error) {
+        res.status(400).json({msg: "Token inválido"})
+    }
+}
+
 //Register User
 app.post('/auth/register', async(req, res) => {
     const {name, email, password, confirmpassword} = req.body
@@ -31,6 +64,79 @@ app.post('/auth/register', async(req, res) => {
     }
     if (!password) {
         return res.status(422).json({ msg: "A senha é Obrigatorio"})
+    }
+    if(password !== confirmpassword){
+        return res.status(422).json({ msg: "As senhas não conferem"})
+    }
+
+    // check if user exist
+    const userExists = await User.findOne({ email: email})
+
+    if (userExists) {
+        return res.status(422).json({ msg: 'Por Favor, utilize outro e-mail'})
+    }
+
+    // create passwor
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = new User({
+        name, 
+        email, 
+        password: passwordHash,
+    })
+
+    try {
+        await user.save()
+
+        res.status(201).json({msg: 'Usuário criado com sucesso'})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({msg: "Aconteceu um erro no servidor, tente novamente mais tarde"})     
+    }
+})
+
+// Login user
+app.post('/auth/login', async(req, res) => {
+    const { email, password } = req.body
+
+    //validations
+    if (!email) {
+        return res.status(422).json({ msg: "O Email é Obrigatorio"})
+    }
+    if (!password) {
+        return res.status(422).json({ msg: "A senha é Obrigatorio"})
+    }
+
+    //check if user exists
+    const user = await User.findOne({ email: email})
+
+    if (!user) {
+        return res.status(404).json({ msg: "Usuario não encontrado"})
+    }
+    
+    //check if password match
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword){
+        return res.status(422).json({ msg: "Senha inválida!"})
+    }
+
+    try {
+        const secret = process.env.SECRET
+
+        const token = jwt.sign(
+            {
+            id: user._id
+            },
+            secret,
+        )
+
+        res.status(200).json({msg: 'Autenticação realizada com sucesso', token})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({msg: "Aconteceu um erro no servidor, tente novamente mais tarde"})     
     }
 })
 
